ba392c51ae3a2001cf19a2707f018c2348fdb987
Denis Alibašić <denis.alibasic22@gmail.com>
2017-02-10 13:18:46 +0100

    Novi screenserver
    Screenserver matrix stil

---
+++ ScreenSaverMatrix/ScreenSaverMatrix/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ScreenSaverMatrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(284, 261);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseMove);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ ScreenSaverMatrix/ScreenSaverMatrix/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaverMatrix
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int xPosition0 = Cursor.Position.X;
+        private int yPosition0 = Cursor.Position.Y;
+
+
+        string[] brojeviIslova = new[] { "0"," ", "1", "2"," ", "3"," ", "4", "5", "6", "7"," ", " ", "8", "9", " ", "A", " ", "B", "C"," ", " ", "D", "E", "F", " " };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            generateMyData();
+            timer = new Timer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = 200;
+            timer.Start();
+        }
+
+        private void richTextBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (xPosition0 - Cursor.Position.X < -10
+                || yPosition0 - Cursor.Position.Y < -10)
+            {
+                this.Close();
+            }
+        }
+
+
+        public void generateMyData()
+        {
+            richTextBox1.Text = "";
+            Form1 forma = new Form1();
+            Random random = new Random();
+
+            int width = forma.Width;
+            int height = forma.Height;
+            var y = new int[width];
+
+            while (richTextBox1.TextLength < height + width + richTextBox1.Height)
+            {
+                int start = random.Next(0, brojeviIslova.Length);
+
+                int length = richTextBox1.TextLength;
+                richTextBox1.AppendText(brojeviIslova[start]);
+
+
+
+                if (start > 6)
+                {
+                    richTextBox1.SelectionStart = length;
+                    richTextBox1.SelectionLength = brojeviIslova[start].Length;
+                    richTextBox1.SelectionColor = Color.FromArgb(0, 192, 0);
+                }
+                else
+                {
+                    richTextBox1.SelectionStart = length;
+                    richTextBox1.SelectionLength = brojeviIslova[start].Length;
+                    richTextBox1.SelectionColor = Color.White;
+                }
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            generateMyData();
+        }
+    }
+}
